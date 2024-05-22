c8283178c5ff6d1423d87599e5266f6ac8fb0ee9
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2024-05-22 22:27:20 +0300

    forgotten file

---
+++ src/components/TheFilteredMovieList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import MovieCard from '@/components/MovieCard.vue'
+import { useMovies } from '@/composables/useMovies'
+import { useMoviesStore } from '@/store/movies'
+
+const { filteredMovies } = useMovies()
+const store = useMoviesStore()
+</script>
+
+<template>
+  <main v-if="!store.loading">
+    <div v-if="filteredMovies?.length">
+      <MovieCard v-for="movie in filteredMovies" :movie="movie" :key="movie.title" />
+    </div>
+    <div class="not_found" v-else>No films found</div>
+  </main>
+  <div v-else>Loading...</div>
+</template>
+
+<style scoped>
+main > div {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 56px;
+  justify-content: center;
+  margin-top: 40px;
+}
+
+.not_found {
+  color: #ffffff;
+  font-family: 'Montserrat';
+  font-size: 40px;
+}
+</style>
