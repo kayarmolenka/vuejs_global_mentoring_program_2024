3136ec61fdc6a2f972dcb40dab96ea06e27f4136
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2024-06-06 17:44:59 +0300

    fix build

--- src/composables/__tests__/useMovies.test.ts
+++ src/composables/__tests__/useMovies.test.ts
@@ -3,6 +3,24 @@ import { useMovies } from '@/composables/useMovies'
 import { useMoviesStore } from '@/store/movies'
 import { createPinia, setActivePinia } from 'pinia'
 
+const mockedMovie = {
+  id: 1,
+  title: 'Movie 1',
+  posterurl: '',
+  genres: ['Action', 'Comedy'],
+  actors: [],
+  releaseDate: '2000-01-01',
+  storyline: '',
+  ratings: [],
+  movieUrl: '',
+  year: '2000',
+  poster: '',
+  contentRating: '',
+  duration: '',
+  averageRating: 0,
+  imdbRating: 0
+}
+
 describe('useMovies', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
@@ -20,9 +38,12 @@ describe('useMovies', () => {
   it('returns movies with the chosen genre sorted by the store sortBy property', async () => {
     const store = useMoviesStore()
     store.movies = [
-      { title: 'Movie 1', genres: ['Action', 'Comedy'], releaseDate: '2000-01-01' },
-      { title: 'Movie 2', genres: ['Drama'], releaseDate: '1999-01-01' },
-      { title: 'Movie 3', genres: ['Action'], releaseDate: '2001-01-01' }
+      {
+        ...mockedMovie,
+        ...{ title: 'Movie 1', genres: ['Action', 'Comedy'], releaseDate: '2000-01-01' }
+      },
+      { ...mockedMovie, ...{ title: 'Movie 2', genres: ['Drama'], releaseDate: '1999-01-01' } },
+      { ...mockedMovie, ...{ title: 'Movie 3', genres: ['Action'], releaseDate: '2001-01-01' } }
     ]
     store.chosenMovieGenre = ['Action']
     store.sortBy = 'Release date'
@@ -30,17 +51,20 @@ describe('useMovies', () => {
     const { moviesByGenre } = useMovies()
 
     expect(moviesByGenre.value).toStrictEqual([
-      { title: 'Movie 3', genres: ['Action'], releaseDate: '2001-01-01' },
-      { title: 'Movie 1', genres: ['Action', 'Comedy'], releaseDate: '2000-01-01' }
+      { ...mockedMovie, ...{ title: 'Movie 3', genres: ['Action'], releaseDate: '2001-01-01' } },
+      {
+        ...mockedMovie,
+        ...{ title: 'Movie 1', genres: ['Action', 'Comedy'], releaseDate: '2000-01-01' }
+      }
     ])
   })
 
   it('returns movies with the chosen genre sorted by rating', async () => {
     const store = useMoviesStore()
     store.movies = [
-      { title: 'Movie 1', genres: ['Action'], imdbRating: 7.5 },
-      { title: 'Movie 2', genres: ['Action'], imdbRating: 8.0 },
-      { title: 'Movie 3', genres: ['Action'], imdbRating: 7.0 }
+      { ...mockedMovie, ...{ title: 'Movie 1', genres: ['Action'], imdbRating: 7.5 } },
+      { ...mockedMovie, ...{ title: 'Movie 2', genres: ['Action'], imdbRating: 8.0 } },
+      { ...mockedMovie, ...{ title: 'Movie 3', genres: ['Action'], imdbRating: 7.0 } }
     ]
     store.chosenMovieGenre = ['Action']
     store.sortBy = 'Rating'
@@ -48,9 +72,9 @@ describe('useMovies', () => {
     const { moviesByGenre } = useMovies()
 
     expect(moviesByGenre.value).toStrictEqual([
-      { title: 'Movie 2', genres: ['Action'], imdbRating: 8.0 },
-      { title: 'Movie 1', genres: ['Action'], imdbRating: 7.5 },
-      { title: 'Movie 3', genres: ['Action'], imdbRating: 7.0 }
+      { ...mockedMovie, ...{ title: 'Movie 2', genres: ['Action'], imdbRating: 8.0 } },
+      { ...mockedMovie, ...{ title: 'Movie 1', genres: ['Action'], imdbRating: 7.5 } },
+      { ...mockedMovie, ...{ title: 'Movie 3', genres: ['Action'], imdbRating: 7.0 } }
     ])
   })
 })
